31c7f1a86a4ea71c384a14f3f2e4713540cb25ab
dibyajeetk <dibyajeet.creates@gmail.com>
2025-07-14 20:02:58 +0530

    Started building functionality

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # simple-calculator
-Calculator project using HTML, CSS and JavaScript
+Calculator project using HTML, CSS and JavaScript.
+
+-design inspiration: https://dribbble.com/shots/13930478-DailyUI-004-A-Calculator
+
+-typo-digit font: https://www.1001fonts.com/typo-digit-demo-font.html
--- src/script.js
+++ src/script.js
@@ -2,4 +2,51 @@ const screen = document.querySelector("#screen");
 const screenOutput = document.createElement("p");
 screen.appendChild(screenOutput);
 screenOutput.setAttribute("id", "screenOutput")
-screenOutput.textContent = "0"
+screenOutput.textContent = '0';
+
+
+// 
+let inputA = 0;
+let inputB = 0;
+let func = 0;
+
+function operate (a, b, fn) {
+    inputA = a;
+    inputB = b;
+    func = fn;
+    if (func === 'add') {
+        screenOutput.textContent = addition();
+    } else if (func === 'sub') {
+        screenOutput.textContent = substraction();
+    }
+}
+// test: chain operation
+let num = [];
+function test (fn, ...args) {
+    let func = fn;
+    let output = 0;
+    if (func === 'add') {
+        output = args.reduce((total, num) => total += num);
+        screenOutput.textContent = output;
+    } else if (func === 'sub') {
+        output = args.reduce((total, num) => total -= num);
+        screenOutput.textContent = output;
+    }
+}
+
+
+function addition () {
+    return inputA + inputB;
+}
+
+function substraction (a, b) {
+    return inputA - inputB;
+}
+
+function multiplication (a, b) {
+    return a * b;
+}
+
+function division (a, b) {
+    return b / a;
+}
