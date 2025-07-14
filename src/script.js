0c4397a4d2b2240679a9b4993dbe52e9043c6042
dibyajeetk <dibyajeet.creates@gmail.com>
2025-07-14 17:26:23 +0530

    feat: Look and feel updated! - retro theme ui - inspired from physical calculator - screen completed

---
+++ src/script.js
@@ -0,0 +1,5 @@
+const screen = document.querySelector("#screen");
+const screenOutput = document.createElement("p");
+screen.appendChild(screenOutput);
+screenOutput.setAttribute("id", "screenOutput")
+screenOutput.textContent = "0"
