b74b35698508c81b8cec4aa53d030d5e0e95504a
dibyajeetk <dibyajeet.creates@gmail.com>
2025-07-16 22:22:15 +0530

    started working on functionality

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ const screen = document.querySelector("#screen");
 const screenCurrentOperation = document.createElement("p");
 screen.appendChild(screenCurrentOperation);
 screenCurrentOperation.setAttribute("id", "screenCurrentOperation");
-screenCurrentOperation.textContent = 'lorem ipsum';
+screenCurrentOperation.textContent = '-';
 
 // result
 const screenOutput = document.createElement("p");
@@ -12,26 +12,41 @@ screen.appendChild(screenOutput);
 screenOutput.setAttribute("id", "screenOutput");
 screenOutput.textContent = '0';
 
+// global variable
+let numX = '0';
+let numY = 0;
+let func = 0;
+
 // User input
 const keyboard = document.querySelector("#keyboard");
 //slect all keys
 const keys = Array.from(document.querySelectorAll('.group .btn'));
+const numberKeys = keys.filter(key => {
+    const nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    return nums.includes(key.textContent);
+});
 
 keys.forEach(key => {
     key.addEventListener('click', (e) => {
         let target = e.target;
-        if (target.textContent == 'ac') {
+        console.log(target.title)
+        if (target.textContent === 'ac') {
             screenCurrentOperation.textContent = '-';
             screenOutput.textContent = '0';
-        } else
-        screenCurrentOperation.textContent = target.textContent;
+            numX = '0';
+        } else if (numberKeys.find(key => key === target)) {
+            let input = '';
+            input += target.textContent;
+            numX = numX.concat(input);
+            numX = numX.slice(1);
+            screenOutput.textContent = numX;
+            console.log(numX)
+        } else if (target.title === 'backspace') {
+            inputA = inputA.slice(0, -1);
+            screenOutput.textContent = inputA;
+        }
     });
 });
-
-// 
-let inputA = 0;
-let inputB = 0;
-let func = 0;
 
 function operate (a, b, fn) {
     inputA = a;
