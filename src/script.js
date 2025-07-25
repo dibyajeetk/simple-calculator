cdea9b919034507af122d06baf5e9b25257b04c2
dibyajeetk <dibyajeet.creates@gmail.com>
2025-07-25 13:11:01 +0530

    rewritten logic from scratch

--- src/script.js
+++ src/script.js
@@ -1,92 +1,69 @@
-const keys = document.querySelectorAll(".group .btn");
-const screen = document.querySelector("#screen");
-const displayText = document.createElement('p'); 
-const subText = document.createElement('p');
-screen.appendChild(subText);
-subText.setAttribute('id', 'subText')
-subText.textContent = '-';
-screen.appendChild(displayText);
-displayText.textContent = 0;
-displayText.setAttribute("id", "displayText");
+const screenUI = document.querySelector('#screen'); // screen div elment
+
+const outputUI = document.createElement('p'); // output p element
+outputUI.textContent = 0;
+outputUI.setAttribute('id', 'output');
+screenUI.appendChild(outputUI);
+
+let userInputX = 0; // stores first user input
+let userInputY = 0; // stores second user input
+let userOperation = 0; // stores users intended action
+let rawInput = 0;
 
 
-let operation = {}; // stores entire user operation under keys numX, operation, NumY
-let input = ''; // stores raw Input from user
-let nums = [0, 1, 2 ,3, 4, 5, 6, 7, 8, 9, '.'];
-let operators = ['+', '−', '×', '÷', '%', 'U+2212'];
+// calculator operators & functions
 
-// User Input Events Management
-// Need a way to take input for 1st, 2nd numbers and operation
-// update display text: 1st num + '' + operation + '' + 2nd num
-// 
-keys.forEach(key => {
-    key.addEventListener('click', (e) => {
-        let target = e.target;
-        
-        let numX = 0;
-        let numY = 0;
-        let action = 0;
-
-        if ((nums.map(n => n.toString())).includes(target.textContent)) {
-            input += (target.textContent); //.trim();
-            if (input.includes('.')) {
-                numX = parseFloat(input);
-                displayText.textContent = input;
-            } else {
-                numX = parseInt(input);
-                displayText.textContent = numX;
-            }
-            console.log(numX);
-        } else if (operators.includes(target.textContent)) {
-            let mapping = {
-                '+': sum(),
-            }
-            action = target.textContent;
-            input += action;
-            displayText.textContent = input;
-        } else if (target.textContent === 'ac') {
-            input = '';
-            displayText.textContent = 0;
-        } else if (target.textContent === 'backspace') {
-            input = input.slice(0, -1);
-            displayText.textContent = input;
-        } else if (target.textContent === '=') {
-            input = operate(numX, numY, action);
-            displayText.textContent = input;
-        } else {
-            input = 'rand';
-            displayText.textContent += input;
-        }
-            
-        
-        // let action = operators.find(op => input.includes(op));
-        // let numX = 0;
-        // let numY = 0;
-        // if (!action) {
-        //     numX = input;
-        //     displayText.textContent = numX
-        //     console.log(`numX: ${numX}, action: ${action}, numY: ${numY}`);
-        // } else {
-        //     numX = input.slice(0, input.indexOf(action)).trim();
-        //     numY = input.slice(input.indexOf(action) + 1).trim();
-    
-        //     displayText.textContent = `${numX}${action}${numY}`;
-        //     console.log(`numX: ${numX}, action: ${action}, numY: ${numY}`);
-        // }
-        // formatting raw input
-        // let action = ((operators.filter(n => input.includes(n))).join()).slice(-1); // find operator
-        // let numX = input.slice(0, input.indexOf(action)); // first number(get everynumber before operation)
-        // let numY = input.slice(input.indexOf(action) + 1);
-        // displayText.textContent = numX + '' + action + '' + numY;
-        // console.log(numX, action, numY);
-    });
-});
-
-function operate (x , y, operator) {
-    let value = `${x}, ${y}, ${operator}`;
-    return value
+function operate (x, y, op) {
+    x = parseInt(x);
+    y = parseInt(y);
+    let result = 0;
+    if (op === '+') {
+        result = x + y;
+    } else if (op === '-') {
+        result = x - y;
+    }
+    else if (op === '*') {
+        result = x * y;
+    } else if (op === '/') {
+        result = x / y;
+    } else if (op === '%') {
+        result = x % y;
+    }
+    outputUI.textContent = result;
+    console.log(result)
 };
 
-function sum (x , y) {
-    return x + y
-}
+// keyboard event listner
+const keyboard = document.querySelectorAll('.group .btn');
+
+keyboard.forEach(key => key.addEventListener('click', (e) => {
+    let target = e.target; // each key
+    
+    let nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let ops = ['+', '-', '*', '%', '/'];
+
+    if (target.textContent === '=') {
+        operate(userInputX, userInputY, userOperation);
+    } else if (target.textContent === 'ac') {
+        rawInput = 0;
+        userInputX = 0;
+        userInputY = 0;
+        userOperation = 0;
+        outputUI.textContent = 0;
+    } else if (target.title === 'backspace') {
+        rawInput = rawInput.slice(0, -1);
+        userInputX = rawInput.slice(1);
+        outputUI.textContent = userInputX;
+    } else {
+        rawInput += target.textContent;
+        outputUI.textContent = rawInput.slice(1);
+
+        if (ops.some(op => rawInput.includes(op))){
+            userOperation = ops.find(op => rawInput.includes(op));
+            const opIndex = rawInput.indexOf(userOperation);
+            userInputX = rawInput.slice(1, opIndex);
+            userInputY = rawInput.slice(opIndex + 1);
+            outputUI.textContent = userInputX + userOperation + userInputY;
+        }
+    }
+}));
